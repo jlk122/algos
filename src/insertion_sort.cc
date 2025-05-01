758e6e2e434bb0a8def1b8f7497b5524fabd3561
Lukas Karlsson <johnlukas.k@gmail.com>
2025-05-01 22:08:49 +0200

    reverse insertion sort for ints with a unittest

--- src/insertion_sort.cc
+++ src/insertion_sort.cc
@@ -2,7 +2,7 @@
 
 namespace sorting{
 
-void insertionSort(std::vector<int>& v)
+void insertionSort(std::vector<int>& v, bool reverse=false)
 {
 	for (int i = 1; i < v.size(); i++)
 	{
@@ -11,7 +11,7 @@ void insertionSort(std::vector<int>& v)
 
 		// set j to former index(if first iter then j is first element in vector)
 		auto j = i - 1;
-		while (j >= 0 && v[j] > key) // if index valid and element bigger than key. i.e searches where to insert
+		while (j >= 0 && (reverse ? (v[j] < key) : (v[j] > key))) // if index valid and element bigger than key. i.e searches where to insert
 		{
 			// Write current element to next one (in first iter position of key is overwritten, therefore copied into key var)
 			v[j + 1] = v[j];
--- test/insertion_sort_test.cc
+++ test/insertion_sort_test.cc
@@ -8,6 +8,7 @@ struct InsertionSort : public testing::Test {
 
 	std::vector<int> four_elements{2,4,3,1};
 	std::vector<int> four_elements_result {std::vector<int>{1,2,3,4}};
+	std::vector<int> four_elements_result_reverse {std::vector<int>{4,3,2,1}};
 
 	std::vector<int> duplicates{2,4,1,3,1};
 	std::vector<int> duplicates_result {std::vector<int>{1,1,2,3,4}};
@@ -21,6 +22,12 @@ TEST_F(InsertionSort, RunOnFourElements)
 {
 	sorting::insertionSort(four_elements);
 	EXPECT_EQ(four_elements, four_elements_result);
+}
+
+TEST_F(InsertionSort, RunOnFourElementsReverse)
+{
+	sorting::insertionSort(four_elements, true);
+	EXPECT_EQ(four_elements, four_elements_result_reverse);
 }
 
 TEST_F(InsertionSort, RunOnEmpty)
